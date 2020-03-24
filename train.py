43af8a64e657025e1d54e8b57d14f976185ab4ef
EC2 Default User <ec2-user@ip-172-16-13-55.ec2.internal>
2020-03-24 09:52:20 +0000

    Edit training

---
+++ train.py
@@ -0,0 +1,49 @@
+import os
+import numpy as np
+import pandas as pd
+import math
+import csv
+
+import transformers
+from tqdm.notebook import trange, tqdm
+
+from sentence_transformers import SentenceTransformer, SentencesDataset, losses
+from sentence_transformers.readers import STSDataReader, TripletReader
+from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator, BinaryEmbeddingSimilarityEvaluator, SequentialEvaluator
+from sentence_transformers.readers.InputExample import InputExample
+
+import torch
+from torch.utils.data import DataLoader, RandomSampler
+
+from scipy.spatial.distance import cdist
+
+torch.cuda.empty_cache()
+
+my_model_path = 'msmarco/models/test_model4'
+
+model_1 = SentenceTransformer('roberta-large-nli-stsb-mean-tokens')
+
+dev_dataloader = torch.load(os.path.join('msmarco/models/test_model4', 'dev_dataloader.pth'))
+train_dataloader =  torch.load(os.path.join('msmarco/models/test_model4', 'train_dataloader.pth'))
+
+evaluator1 = BinaryEmbeddingSimilarityEvaluator(dev_dataloader)
+evaluator2 = EmbeddingSimilarityEvaluator(dev_dataloader)
+evaluator = SequentialEvaluator([evaluator1, evaluator2])
+
+optimizer_class = transformers.AdamW
+optimizer_params = {'lr': 2e-5, 'eps': 1e-6, 'correct_bias': False}
+train_loss = losses.CosineSimilarityLoss(model=model_1)
+
+num_epochs = 100
+warmup_steps = math.ceil(len(train_dataloader.dataset)*num_epochs / train_dataloader.batch_size*0.05) #5% of train data for warm-up
+
+model_1.fit(train_objectives=[(train_dataloader, train_loss)],
+          evaluator=evaluator,
+          epochs=num_epochs,
+          steps_per_epoch=1000,
+          warmup_steps=warmup_steps,
+          optimizer_class=optimizer_class,
+          optimizer_params=optimizer_params,
+          output_path=os.path.join(my_model_path, 'model4')) # works only when you have an evaluator
+
+model_1.save(os.path.join(my_model_path, 'model4_final'))
